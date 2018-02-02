f4f0dd6910bc5ea00feb11c96a341446dd5f4de2
DaveLindberg <dave@davelindberg.com>
2018-02-01 23:44:59 -0500

    modified SASS files for website

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import User from './User';
 import Projects from './Projects';
 import './style/main.css';
 
 class Portfolio extends Component {
   render() {
-
-    const API_KEY = this.props.apiKey === undefined ? 'LDGQKFP7dsmkhIKUAGG67ChSDASj1cWD' : this.props.apiKey;
+    const API_KEY =
+      this.props.apiKey === undefined
+        ? 'LDGQKFP7dsmkhIKUAGG67ChSDASj1cWD'
+        : this.props.apiKey;
 
     let baseUrl = `https://api.behance.net/v2/users/${this.props.user}`;
-    let urlUser = `${baseUrl}?client_id=${API_KEY}`;
     let urlProjects = `${baseUrl}/projects?client_id=${API_KEY}`;
 
     return (
-      <div className="Portfolio">
-        <User url={urlUser} />
-        <Projects url={urlProjects} showFeatured={this.props.showFeatured} /> 
+      <div
+        className="Portfolio"
+        style={{
+          gridColumn: `1 / 4`,
+          gridRow: `7 / 7`
+        }}
+      >
+        <Projects url={urlProjects} showFeatured={this.props.showFeatured} />
       </div>
     );
   }
--- src/components/Portfolio/Projects.js
+++ src/components/Portfolio/Projects.js
@@ -8,20 +8,19 @@ class User extends Component {
 
     this.state = {
       projects: []
-    }
+    };
   }
 
   componentDidMount() {
-
     jsonp(this.props.url, null, (err, data) => {
       if (err) {
         console.log(err);
       } else {
-
-        let projects = [], id, higherNumber = 0;
+        let projects = [],
+          id,
+          higherNumber = 0;
 
         data.projects.forEach(project => {
-
           //verify if this project has the higher view number
           if (project.stats.views > higherNumber) {
             higherNumber = project.stats.views;
@@ -33,17 +32,18 @@ class User extends Component {
           if (!imageBlocked.test(project.covers[202])) {
             let data = {
               name: project.name,
-              published_on: () => moment(project.published_on * 1000).calendar(),
+              published_on: () =>
+                moment(project.published_on * 1000).calendar(),
               image: project.covers[404] || project.covers[202],
               stats: {
                 appreciations: project.stats.appreciations,
-                views: project.stats.views,
+                views: project.stats.views
               },
               created_on: project.created_on,
               url: project.url,
-              featured: () => project.id === id ? true : false,
+              featured: () => (project.id === id ? true : false),
               fields: project.fields
-            }
+            };
 
             projects.push(data);
           }
@@ -52,51 +52,58 @@ class User extends Component {
         this.setState({ projects });
       }
     });
-
   }
 
   render() {
-
     let projects = this.state.projects.map((project, i) => {
-
-      let showFeatured = () => this.props.showFeatured ? (project.featured() ? 'Card__featured' : '') : '';
+      let showFeatured = () =>
+        this.props.showFeatured
+          ? project.featured() ? 'Card__featured' : ''
+          : '';
 
       let fields = project.fields.map((field, i) => {
         return (
-          <span className="badge" key={i}>{field}</span>
+          <span className="badge" key={i}>
+            {field}
+          </span>
         );
       });
 
       return (
-        <article className={`Card Grid__cell xs-6 sm-4 md-3 lg-2 ${showFeatured()}`} key={i}>
+        <article
+          className={`Card Grid__cell xs-6 sm-4 md-3 lg-2 ${showFeatured()}`}
+          key={i}
+        >
           <img className="Card__media" src={project.image} alt="project" />
           <div className="Card__details">
             <div>
-              <i className="material-icons md-12">thumb_up</i> {project.stats.appreciations}
+              <i className="material-icons md-12">thumb_up</i>{' '}
+              {project.stats.appreciations}
             </div>
             <div>
-              <i className="material-icons md-12">visibility</i> {project.stats.views}
+              <i className="material-icons md-12">visibility</i>{' '}
+              {project.stats.views}
             </div>
           </div>
           <div className="Card__footer">
             <small>published on {project.published_on()}</small>
             <h2>{project.name}</h2>
-            <p>
-              {fields}
-            </p>
+            <p>{fields}</p>
             <div className="Card__footer-footer">
-              <a className="btn btn--primary" href={project.url} target="_blank">more details</a>
+              <a
+                className="btn btn--primary"
+                href={project.url}
+                target="_blank"
+              >
+                more details
+              </a>
             </div>
           </div>
         </article>
       );
     });
 
-    return (
-      <div className="Projects Grid">
-        {projects}
-      </div>
-    );
+    return <div className="Projects Grid">{projects}</div>;
   }
 }
 
